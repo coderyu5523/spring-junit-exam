853b0b64c10874853a36a2028a40c991ab8ba460
coderyu5523 <coderyu5523@naver.com>
2024-02-22 20:05:06 +0900

    아작스 게시글 작성

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -1,14 +1,9 @@
 package shop.mtcoding.blog.board;
 
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,22 +12,30 @@ import java.util.List;
 public class BoardApiController {
     private final BoardRepository boardRepository;
 
-    @GetMapping("/api/boards")
-    public ApiUtil<?> findAll(){
-        List<Board> boardList = boardRepository.selectAll();
-        return new ApiUtil<>(boardList);
-
-    }
     @DeleteMapping("/api/boards/{id}")
     public ApiUtil<?> deleteById(@PathVariable Integer id, HttpServletResponse response){
         Board board = boardRepository.selectOne(id);
-        if(board==null){
+        if(board == null){
             response.setStatus(404);
-            return new ApiUtil<>(404,"해당 게시글을 찾을 수 없습니다.");
+            return new ApiUtil<>(404, "해당 게시글을 찾을 수 없습니다");
         }
-        boardRepository.deleteById(id);
 
+        boardRepository.deleteById(id);
         return new ApiUtil<>(null);
     }
 
+
+    @GetMapping("/api/boards")
+    public ApiUtil<?> findAll(HttpServletResponse response) {
+        //response.setStatus(200);
+        List<Board> boardList = boardRepository.selectAll();
+        return new ApiUtil<>(boardList); // MessageConverter
+    }
+
+    @PostMapping("/api/boards")
+    public ApiUtil<?> write(@RequestBody BoardRequest.WriteDTO requestDTO){ //@RequestBody 를 붙이면 제이슨형태로 받을 수 있음
+        boardRepository.insert(requestDTO);
+
+        return new ApiUtil<>(null);
+    }
 }
---
+++ src/main/java/shop/mtcoding/blog/board/BoardRequest.java
@@ -0,0 +1,14 @@
+package shop.mtcoding.blog.board;
+
+import lombok.Data;
+
+public class BoardRequest {
+    @Data
+    public static class WriteDTO{
+
+        private String title ;
+        private String content;
+        private String author;
+    }
+
+}
